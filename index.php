ff825cf6b558c1149e090591440317b9c116816d
Jamie Wagner <jamie@kajabi.com>
2020-08-08 02:49:03 -0700

    Moving site over

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php date_default_timezone_set('America/Los_Angeles'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="So we made some vinyl records. You can buy them. Or you can download them. We really don't give a shit." />
+	<meta name="keywords" content="noisycrane, noisy crane, localnatives, local natives, records, good records, vinyl records, free downloads, the union line, local natives record" />
+	<link rel="stylesheet" href="/css/styles.css" type="text/css" media="screen" />
+	
+	<title>FORSKINNYSTEVE ::: Buy This Shit ::: FORSKINNYSTEVE</title>
+	
+</head>
+
+<body>
+<div id="container">
+	
+	<div id="header">
+		<h1>forskinnysteve.</h1>
+	</div><!-- Header -->
+
+	<div id="middle">
+		<p id="info">Buy it for 5 bucks or Download it for Free.<br /><strong>The Choice is Yours.</strong></p>
+		<div class="record">
+			<h2>Local Natives / The Union Line</h2>
+			<h3>Split 7&rdquo;</h3>
+			<img src="images/local-natives-local_natives.jpg" width="300" height="300" alt="Local Natives / The Union Line Split" />
+			<div class="links">
+				<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post"> 
+				    <input type="image" src="/images/buy_vinyl_button.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"> 
+				    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"> 
+				    <input type="hidden" name="add" value="1"> 
+				    <input type="hidden" name="cmd" value="_cart"> 
+				    <input type="hidden" name="business" value="mikesnowmonkey@yahoo.com"> 
+				    <input type="hidden" name="item_name" value="The Union Line - The Union Line/Local Natives Split"> 
+				    <input type="hidden" name="item_number" value=""> 
+				    <input type="hidden" name="amount" value="5"> 
+				    <input type="hidden" name="no_shipping" value="0"> 
+				    <input type="hidden" name="logo_custom" value="/images/buy_vinyl_button.jpg"> 
+				    <input type="hidden" name="no_note" value="1"> 
+				    <input type="hidden" name="currency_code" value="USD"> 
+				    <input type="hidden" name="lc" value="US"> 
+				    <input type="hidden" name="bn" value="PP-ShopCartBF"> 
+				    </form>
+				<a href="/downloads/FSS001.zip" title="Download this Shit Now!" class="download">Download Now!</a>
+			</div>
+		</div>
+		
+		<div class="record">
+			<h2>Noisycrane</h2>
+			<h3>Bleed Blood Red 7&rdquo;</h3>
+			<img src="images/noisycrane-bleed_blood_red.jpg" width="300" height="300" alt="Noisycrane Bleed Blood Red" />
+			<div class="links">
+				<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post"> 
+				    <input type="image" src="/images/buy_vinyl_button.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"> 
+				    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"> 
+				    <input type="hidden" name="add" value="1"> 
+				    <input type="hidden" name="cmd" value="_cart"> 
+				    <input type="hidden" name="business" value="mikesnowmonkey@yahoo.com"> 
+				    <input type="hidden" name="item_name" value="Noisycrane - Bleed Blood Red 7"> 
+				    <input type="hidden" name="item_number" value=""> 
+				    <input type="hidden" name="amount" value="5"> 
+				    <input type="hidden" name="no_shipping" value="0"> 
+				    <input type="hidden" name="logo_custom" value="/images/buy_vinyl_button.jpg"> 
+				    <input type="hidden" name="no_note" value="1"> 
+				    <input type="hidden" name="currency_code" value="USD"> 
+				    <input type="hidden" name="lc" value="US"> 
+				    <input type="hidden" name="bn" value="PP-ShopCartBF"> 
+				    </form>
+				<a href="/downloads/FSS002.zip" title="Download this Shit Now!" class="download">Download Now!</a>
+			</div>
+		</div>
+	</div><!-- Middle -->
+	
+	<div id="footer">
+		<p>Copyright <?php print date('Y'); ?> - 2008 &copy; forskinnysteve ::: <a href="mailto: info@forskinnysteve.com" title="Email Us Asshole">Contact Us</a></p>
+	</div>
+
+</div><!-- Container -->
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-2019202-20");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
